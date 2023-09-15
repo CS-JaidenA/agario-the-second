a52f1b1249ba856ae3e952204bc5a1491825ec75
Jaiden Appiah <jaidenappiah@gmail.com>
2023-09-14 20:02:20 -0600

    update splitting and add size counter

--- public/components.js
+++ public/components.js
@@ -1,3 +1,29 @@
+import draw from "./manager@draw.js";
+import mouse from "./manager@mouse.js";
+
+/** @type {number} */
+const speed = 5;
+
+/** @type {HTMLCanvasElement} */
+let cnv;
+
+/** @type {CanvasRenderingContext2D} */
+let ctx;
+
+/**
+ * @param {HTMLCanvasElement} canvas
+ * @param {CanvasRenderingContext2D} context
+ * @returns {undefined}
+ */
+function init(canvas, context) {
+	cnv = canvas;
+	ctx = context;
+
+	// initialize dependencies with updated information
+
+	draw.init(ctx);
+}
+
 /**
  * @typedef {object} Blob
  * @property {number} x
@@ -7,11 +33,13 @@
  */
 
 class Blob {
-	/** @type {number} */
-	x;
+	/** @type {object} */
+	x = {};
 
-	/** @type {number} */
-	y;
+	/** @type {object} */
+	y = {};
+
+	id;
 
 	/** @type {number} */
 	radius;
@@ -20,13 +48,23 @@ class Blob {
 	 * @param {number} x
 	 * @param {number} y
 	 * @param {number} radius
+	 * @param {number} xMomentum
+	 * @param {number} yMomentum
 	 */
-	constructor(x, y, radius) {
-		this.x = x;
-		this.y = y;
+	constructor(x, y, radius, xMomentum = 0, yMomentum = 0) {
+		this.x.pos = x;
+		this.y.pos = y;
 
 		this.radius = radius;
+
+		this.x.momentum = xMomentum;
+		this.y.momentum = yMomentum;
+
+		this.id = crypto.randomUUID();
 	}
+
+	static MOMENTUM			= 28;
+	static MIN_BLOB_SIZE	= 50 ;
 }
 
 class Player {
@@ -36,10 +74,89 @@ class Player {
 	/** @type {string} */
 	colour;
 
+	/** @type {boolean} */
+	spacePressed = false;
+
+	/** @returns {undefined} */
 	split() {
 		this.blobs.forEach(blob => {
+			if (blob.radius / 2 < Blob.MIN_BLOB_SIZE) return;
+
 			blob.radius /= 2;
-			this.blobs.push(new Blob(blob.x + 100, blob.y + 100, blob.radius));
+			this.blobs.push(new Blob(
+				blob.x.pos,
+				blob.y.pos,
+				blob.radius,
+
+				// adjust momentum to have same ratio as speed for correct angle
+
+				blob.x.speed > blob.y.speed ? Blob.MOMENTUM : blob.x.speed / blob.y.speed * Blob.MOMENTUM,
+				blob.y.speed > blob.x.speed ? Blob.MOMENTUM : blob.y.speed / blob.x.speed * Blob.MOMENTUM,
+			));
+		});
+	}
+
+	/** @returns {undefined} */
+	draw() {
+		this.blobs.forEach(blob => {
+			// calculate mouse coordinates
+
+			const mouseX = mouse.x * cnv.width;
+			const mouseY = mouse.y * cnv.height;
+
+			// calculate distance between blob and mouse coordinates
+
+			const distanceX = Math.abs(mouseX - blob.x.pos);
+			const distanceY = Math.abs(mouseY - blob.y.pos);
+
+			// speeds are adjusted so that blob x/y coords meet mouse at same time
+			// adjust speed only if the blob is not already centered
+
+			if (distanceX > speed + 1 && distanceY > speed + 1) {
+				blob.x.speed = distanceX > distanceY ? speed : speed * distanceX / distanceY;
+				blob.y.speed = distanceY > distanceX ? speed : speed * distanceY / distanceX;
+			} else {
+				blob.x.speed = 0;
+				blob.y.speed = 0;
+			}
+
+			// add momentum
+
+			blob.x.speed += blob.x.momentum;
+			blob.y.speed += blob.y.momentum;
+
+			// decrease momentum
+			// set to 0 if applicable in case momentum was a decimal
+
+			if (blob.x.momentum > 0)
+				blob.x.momentum--;
+			else blob.x.momentum = 0;
+
+			if (blob.y.momentum > 0)
+				blob.y.momentum--;
+			else blob.y.momentum = 0;
+
+			// move blob
+
+			blob.x.pos += blob.x.speed * (mouseX > blob.x.pos ? 1 : -1);
+			blob.y.pos += blob.y.speed * (mouseY > blob.y.pos ? 1 : -1);
+
+			draw.circle(blob.x.pos, blob.y.pos, blob.radius, this.colour);
+
+			// write size
+
+			const size = String(blob.radius);
+			const metrics = ctx.measureText(size);
+
+			ctx.font		= "48px serif";
+			ctx.fillStyle	= "white";
+			ctx.strokeStyle = "black";
+
+			ctx.fillText(
+				String(blob.radius),
+				blob.x.pos - (metrics.actualBoundingBoxLeft + metrics.actualBoundingBoxRight) / 2,
+				blob.y.pos + (metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent) / 2
+			);
 		});
 	}
 
@@ -52,10 +169,16 @@ class Player {
 		this.blobs.push(new Blob(x, y, radius));
 		this.colour = colour;
 
+		addEventListener("keyup", e => {
+			if (e.code == "Space") this.spacePressed = false;
+		});
+
 		addEventListener("keydown", e => {
-			if (e.code == "Space") this.split();
+			if (this.spacePressed || e.code != "Space") return;
+			this.spacePressed = false;
+			this.split();
 		});
 	}
 }
 
-export default { Player };
+export default { init, Player };
--- public/index.js
+++ public/index.js
@@ -9,12 +9,20 @@ const cnv = document.getElementById("canvas");
 /** @type {CanvasRenderingContext2D} */
 const ctx = cnv.getContext("2d");
 
+// scale canvas
+
+window.addEventListener("resize", () => {
+	cnv.width	= Math.floor(window.innerWidth * window.devicePixelRatio);
+	cnv.height	= Math.floor(window.innerHeight * window.devicePixelRatio);
+});
+
+window.dispatchEvent(new Event("resize"));
+
 // setup
 
-draw.init(ctx);
+components.init(cnv, ctx);
 
-const speed = 6;
-const player = new components.Player(mouse.x * canvas.width, mouse.y * canvas.height, 50, colours.random());
+const player = new components.Player(canvas.width / 2, canvas.height / 2, 100, colours.random());
 
 requestAnimationFrame(update);
 
@@ -28,41 +36,7 @@ function update() {
 	const mouseX = mouse.x * canvas.width;
 	const mouseY = mouse.y * canvas.height;
 
-	for (let blob of player.blobs) {
-		// distance between blob and mouse coordinates
-
-		const distanceX = Math.abs(mouseX - blob.x);
-		const distanceY = Math.abs(mouseY - blob.y);
-
-		// speeds adjusted so blob coords meet mouse coords at same time
-
-		let adjustedSpeedX;
-		let adjustedSpeedY;
-
-		// stop moving the player if the player is about centered
-
-		if (distanceX < 2 && distanceY < 2) {
-			adjustedSpeedX = 0;
-			adjustedSpeedY = 0;
-		} else {
-			adjustedSpeedX = distanceX > distanceY ? speed : speed * distanceX / distanceY;
-			adjustedSpeedY = distanceY > distanceX ? speed : speed * distanceY / distanceX;
-		}
-
-		blob.x += adjustedSpeedX * (mouse.x * canvas.width > blob.x ? 1 : -1);
-		blob.y += adjustedSpeedY * (mouse.y * canvas.height > blob.y ? 1 : -1);
-
-		draw.circle(blob.x, blob.y, blob.radius, player.colour);
-	}
+	player.draw();
 
 	requestAnimationFrame(update);
 }
-
-// scale canvas
-
-window.addEventListener("resize", () => {
-	cnv.width	= Math.floor(window.innerWidth * window.devicePixelRatio);
-	cnv.height	= Math.floor(window.innerHeight * window.devicePixelRatio);
-});
-
-window.dispatchEvent(new Event("resize"));
