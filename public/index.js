656c2d49a53b5328126b9ed9c6bd405224ab2d21
Jaiden Appiah <j.appiah@EDU.epsb.ca>
2023-10-13 14:54:06 -0600

    refactoring progress

--- public/index.js
+++ public/index.js
@@ -9,16 +9,19 @@ let uuid = '';
 ws.addEventListener("message", e => {
 	const message = JSON.parse(e.data);
 
-	console.log(100, message.pack, message.uuid);
-	console.log(200, pack);
-
-	if (message.pack)
-		pack = { ...pack, ...message.pack };
+	// console.log(100, message.pack, message.uuid);
+	// console.log(200, pack);
 
 	if (message.uuid)
-		uuid = uuid;
+		uuid = message.uuid;
 
-	console.log(300, pack);
+	if (message.pack) {
+		console.log(100, message.pack);
+		pack = { ...pack, ...message.pack };
+		requestAnimationFrame(update);
+	}
+
+	console.log(300, uuid);
 });
 
 /** @type {HTMLCanvasElement} */
@@ -70,76 +73,87 @@ const draw = {
 
 // draw canvas
 
-function update() {
-	if (!pack) return;
+const gridColour     = "#313131";
+const gridBoxSize   = 40;
+const gridThickness = 1;
 
+function update() {
 	ctx.clearRect(0, 0, cnv.width, cnv.height);
 
 	const mainPlayerBlob = pack.players[uuid].blobs[0];
 
-	// draw grid
-
-	const lineWidth   = 1;
-	const lineColor   = "#313131";
-	const gridboxSize = 40;
-
-	const offsetX = cnv.width - mainPlayerBlob.mass / 2 + (mainPlayerBlob.x / pack.width * cnv.width);
-
-	// border lines
-
-	const borderX = gridboxSize * pack.width;
-	const borderY = gridboxSize * pack.height;
-
-	draw.line([borderX, 0], [borderX, borderY], lineColor, lineWidth);
-	draw.line([0, borderY], [borderX, borderY], lineColor, lineWidth);
-
-	// vertical lines
-
-	for (let i = 1; i < pack.width; i++) {
-		const x = gridboxSize * i;
-
-		draw.line(
-			[x, 0],
-			[x, Math.min(cnv.height, gridboxSize * pack.height)],
-			lineColor,
-			lineWidth,
-		);
-	}
-
-	// horizontal lines
-
-	for (let i = 1; i < pack.height; i++) {
-		const y = gridboxSize * i;
-
-		draw.line(
-			[0, y],
-			[Math.min(cnv.width, gridboxSize * pack.width), y],
-			lineColor,
-			lineWidth,
-		);
-	}
-
-	for (const player of Object.values(pack.players)) {
-		const blob = player.blobs[0];
-
-		console.log(blob.x, blob.y);
-
-		// draw.circ(
-		// 	blob.x * gridboxSize,
-		// 	blob.y * gridboxSize,
-		// 	blob.mass,
-		// 	player.colour,
-		// );
-
-		draw.circ(
-			cnv.width  / 2,
-			cnv.height / 2,
-			blob.mass,
-			player.colour,
-		);
-	}
-
-	requestAnimationFrame(update);
+	const gridWidth  = gridBoxSize * pack.width;
+	const gridHeight = gridHeight * pack.height;
 }
 
-requestAnimationFrame(update);
+// function update() {
+// 	if (!pack) return;
+
+// 	ctx.clearRect(0, 0, cnv.width, cnv.height);
+
+// 	const mainPlayerBlob = pack.players[uuid].blobs[0];
+
+// 	// draw grid
+
+// 	const lineWidth   = 1;
+// 	const lineColor   = "#313131";
+// 	const gridboxSize = 40;
+
+// 	console.log(10, mainPlayerBlob);
+
+// 	const offsetX = (gridboxSize * pack.width) - (mainPlayerBlob.x / pack.width * cnv.width);
+
+// 	// border lines
+
+// 	const borderX = gridboxSize * pack.width;
+// 	const borderY = gridboxSize * pack.height;
+
+// 	draw.line([borderX, 0], [borderX, borderY], lineColor, lineWidth);
+// 	draw.line([0, borderY], [borderX, borderY], lineColor, lineWidth);
+
+// 	// vertical lines
+
+// 	for (let i = 1; i < pack.width; i++) {
+// 		const x = gridboxSize * i;
+
+// 		draw.line(
+// 			[x, 0],
+// 			[x, Math.min(cnv.height, gridboxSize * pack.height)],
+// 			lineColor,
+// 			lineWidth,
+// 		);
+// 	}
+
+// 	// horizontal lines
+
+// 	for (let i = 1; i < pack.height; i++) {
+// 		const y = gridboxSize * i + offsetX;
+
+// 		draw.line(
+// 			[0, y],
+// 			[Math.min(cnv.width, gridboxSize * pack.width), y],
+// 			lineColor,
+// 			lineWidth,
+// 		);
+// 	}
+
+// 	for (const player of Object.values(pack.players)) {
+// 		const blob = player.blobs[0];
+
+// 		// console.log(blob.x, blob.y);
+
+// 		// draw.circ(
+// 		// 	blob.x * gridboxSize,
+// 		// 	blob.y * gridboxSize,
+// 		// 	blob.mass,
+// 		// 	player.colour,
+// 		// );
+
+// 		draw.circ(
+// 			cnv.width  / 2,
+// 			cnv.height / 2,
+// 			blob.mass,
+// 			player.colour,
+// 		);
+// 	}
+// }
